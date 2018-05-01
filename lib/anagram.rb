08430ab019dff464ea315da8c7c78e594eb97d46
Caio Sanches <ccampato@gmail.com>
2018-05-01 20:44:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(ar)
+    anagr = []
+    ar.each do |ar_word|
+      anagr << ar_word.split
+    end
+    if @word.split.sort == anagr.sort
+      anagr.join
+    else
+      []
+    end
+  end
+
+end
